f644f0bd3d6c4e3c5b9b2013ebd362d3c9c686d6
allure13 <igor.cheplyaka@gmail.com>
2023-10-21 14:00:40 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -13,3 +13,37 @@
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 // console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
 
+// Написати функцію getCountryInfo(country), котра буде
+// отримувати назву країни і виводити інформацію про неї
+// China => "Китай - найбільша країна за населенням у світі."
+// Australia => "Австралія - найбільший острів та країна в Океанії."
+// France => "Франція відома своєю гастрономією та мистецтвом."
+// Germany => "Німеччина - найбільша економіка в Європі."
+// Canada => "Канада - друга за площею країна у світі, відома своєю природою."
+// Якщо країни немає, вивести повідомлення "Інформація про цю країну відсутня."
+
+function getCountryInfo(country) {
+  let info;
+  switch (country) {
+    case "China":
+      info = "Китай - найбільша країна за населенням у світі.";
+      break;
+    case "Australia":
+      info = "Австралія - найбільший острів та країна в Океанії.";
+      break;
+    case "France":
+      info = "Франція відома своєю гастрономією та мистецтвом.";
+      break;
+    case "Germany":
+      info = "Німеччина - найбільша економіка в Європі.";
+      break;
+    case "Canada":
+      info = "Канада - друга за площею країна у світі, відома своєю природою.";
+      break;
+    default:
+      info = "Інформація про цю країну відсутня.";
+      break;
+  }
+  return info;
+}
+console.log(getCountryInfo("Ukraine"));
